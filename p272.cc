dcf4b41814acb208cdee9fec0cc4a34b98da9141
Aditya varma <aditya.varma@grabonrent.com>
2017-01-12 23:55:53 +0530

    first commit

---
+++ p272.cc
@@ -0,0 +1,13 @@
+#include<bits/stdc++.h>
+
+using namespace std;
+
+int main()
+{
+    string a;
+            bool flag = true;
+    while(getline(cin, a))
+    {
+        cout << a << endl;
+    }
+}
